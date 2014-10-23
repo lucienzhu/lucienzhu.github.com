7e371d6d4cdd363980b096f6f26cdf604aa0de0c
Gulimujyujyu <g@gulimujyujyu.me>
2014-10-23 19:42:12 +0800

    prepare a new layout

---
+++ js/default.orbit.js
@@ -0,0 +1,14 @@
+$(document).foundation('orbit', {
+animation: 'fade',
+timer_speed: 5000,
+pause_on_hover: false,
+resume_on_mouseout: false,
+animation_speed: 500,
+navigation_arrows: false,
+slide_number: true,
+bullets: false,
+timer: true,
+variable_height: false,
+before_slide_change: function(){},
+after_slide_change: function(){}
+});
